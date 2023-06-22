fa0521ea12321b17a7be9b583c800c4cfb14df31
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 14:57:53 +0500

    Create Logo.tsx

---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+const Logo = () => {
+  return (
+    <div className='flex items-center ml-2'>
+      <img
+        src='/assets/images/SmartCoachLogo.jpg'
+        alt='Smart Coach Logo'
+        width={28}
+        height={28}
+      />
+      <span className='pl-2 font-semibold text-xl'>Smart Coach</span>
+    </div>
+  );
+};
+
+export default Logo;
