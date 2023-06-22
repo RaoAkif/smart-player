a5cf62e9ce5d3161258bc0b206145a1c1ed321d3
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 21:39:41 +0500

    Get Events from PlayerById Query

--- src/features/events/Events.tsx
+++ src/features/events/Events.tsx
@@ -1,4 +1,4 @@
-import { useGetEventsQuery } from "./eventsApiSlice";
+import { useGetPlayerByIdQuery } from "../players/playersApiSlice";
 import { Link } from "react-router-dom";
 
 type EventTypes = "PRACTICE" | "MATCH" | "OTHER";
@@ -16,7 +16,9 @@ interface EventProps {
 }
 
 const Events = () => {
-  const { data: events, isLoading, error } = useGetEventsQuery({});
+  const userId = localStorage.getItem("userId");
+  const { data: players, isLoading, error } = useGetPlayerByIdQuery(userId);
+
   if (isLoading) {
     return <div>Loading events...</div>;
   }
@@ -27,7 +29,12 @@ const Events = () => {
 
   return (
     <>
-      <h1 className="pt-3 pl-10 font-bold text-lg -mb-6 pb-3 text-center pr-10" style={{borderBottom: '1px solid #e5e7eb'}}>Events</h1>
+      <h1
+        className='pt-3 pl-10 font-bold text-lg -mb-6 pb-3 text-center pr-10'
+        style={{ borderBottom: "1px solid #e5e7eb" }}
+      >
+        Events
+      </h1>
       <div className='rounded-xl border border-solid border-gray-300 m-10 pb-3 ml-5 mr-4 pt-3 bg-gray-100'>
         <div>
           <table className='w-full'>
@@ -39,26 +46,29 @@ const Events = () => {
               </tr>
             </thead>
             <tbody>
-              {events.map((event: EventProps) => (
-                <tr
-                  key={event.id}
-                  className='h-16 border-t border-gray-300 bg-white'
-                >
-                  <td className='pl-4'>
-                    <Link
-                      to={`/dashboard/events/${event.id}`}
-                      className='text-blue-700 underline font-bold'
-                    >
-                      {event.title}
-                    </Link>
-                  </td>
-                  <td className='pl-4'>{event.date}</td>
-                  <td className='pl-4'>{event.event_type}</td>
-                </tr>
-              ))}
+              {players?.teams?.map((team: { events: EventProps[]; }) =>
+                team.events.map((event: EventProps) => (
+                  <tr
+                    key={event.id}
+                    className='h-16 border-t border-gray-300 bg-white'
+                  >
+                    <td className='pl-4'>
+                      <Link
+                        to={`/dashboard/events/${event.id}`}
+                        className='text-blue-700 underline font-bold'
+                      >
+                        {event.title}
+                      </Link>
+                    </td>
+                    <td className='pl-4'>{event.date}</td>
+                    <td className='pl-4'>{event.details}</td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
         </div>
+        <div></div>
       </div>
     </>
   );
