7b13670c772482e4a2b5bed910b53a229c15bdd3
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 18:25:46 +0500

    Route to login page

--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const CreateButton = () => {
   return (
     <div className="flex items-center py-2 px-4 border-2 border-blue-700 rounded-md mr-2">
-      <span className="font-bold text-blue-700 text-sm border-blue-700 hover:underline">
+      <Link to="/login" className="font-bold text-blue-700 text-sm border-blue-700 hover:underline">
         Login
-      </span>
+      </Link>
     </div>
   );
 };
