92cafa3232161ad7379c568f0cca24c90d733534
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 16:55:07 +0500

    Update CreateButton.tsx

--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -1,7 +1,7 @@
 const CreateButton = () => {
   return (
     <div className="flex items-center py-2 px-4 border-2 border-blue-700 rounded-md mr-2">
-      <span className="ml-2 font-bold text-blue-700 text-sm border-blue-700 hover:underline">
+      <span className="font-bold text-blue-700 text-sm border-blue-700 hover:underline">
         Login
       </span>
     </div>
