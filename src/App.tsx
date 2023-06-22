b5d7eab753cf2b5f8354848a140c2de7c4a2b618
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 14:58:46 +0500

    Add assets from Smart Coach App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/authComponents/Public'
+import Login from './features/auth/Login';
+import DashLayout from './components/dashComponents/DashLayout'
+import Home from './features/auth/Home'
+import Events from './features/events/Events'
+import EventDetails from './features/events/EventDetails'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <>
-      <h1 className="text-5xl font-bold text-center font-sans mt-52 underline">
-        Hello World!
-      </h1>
-    </>
-  )
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Public />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="dashboard" element={<DashLayout />}>
+
+            <Route index element={<Home />} />
+
+            <Route path="events">
+              <Route index element={<Events />} />
+              <Route path=":eventId" element={<EventDetails />} />
+            </Route>
+
+          </Route>{/* End Dash */}
+        </Route>
+      </Routes>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
