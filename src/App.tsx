63ba4209f2fb20bcb535b91cfd2bc40a2e7e53fa
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-06-22 11:56:21 +0500

    Add Tailwind and Netlify Configurations

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-center font-sans mt-52 underline">
+        Hello World!
+      </h1>
+    </>
+  )
+}
+
+export default App
